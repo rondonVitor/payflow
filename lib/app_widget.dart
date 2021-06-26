627c7d776b354e2ad0f25a481e9443a1d51b8e4a
Vitor Rondon <rondon.vitor@yahoo.com.br>
2021-06-25 21:24:58 -0300

    Added BarcodeScannerPage

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_ml_kit/google_ml_kit.dart';
+import 'package:payflow/modules/barcode_scanner/barcode_scanner_page.dart';
 import 'package:payflow/modules/home/home_page.dart';
 import 'package:payflow/modules/splash/splash_page.dart';
 
@@ -6,11 +9,20 @@ import 'modules/login/login_page.dart';
 import 'shared/themes/app_colors.dart';
 
 class AppWidget extends StatelessWidget {
+
+  AppWidget() {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.portraitUp,
+    ]);
+  }
+
   @override
   Widget build(BuildContext context) {    
     return MaterialApp(
       title: 'Pay Flow',
       theme: ThemeData(
+        primarySwatch: Colors.orange,
         primaryColor: AppColors.primary,
       ),
       initialRoute: "/splash",
@@ -18,6 +30,7 @@ class AppWidget extends StatelessWidget {
         "/splash" : (context) => SplashPage(),
         "/home" : (context) => HomePage(),
         "/login" : (context) => LoginPage(),
+        "/barcode_scanner" : (context) => BarcodeScannerPage(),
       }
     );
   }
--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -66,7 +66,7 @@ class _HomePageState extends State<HomePage> {
             ),
             GestureDetector(
               onTap: () {
-                print("Clicou");
+                Navigator.pushNamed(context, "/barcode_scanner");
               },
               child: Container(
                 width: 56,
